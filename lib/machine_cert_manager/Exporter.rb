41baf05183b195e0b363b1256f66f17e78e0f579
Noah Rosenzweig <noah@aware.vision>
2018-07-09 16:52:46 +0200

    rename exporter.rb

--- lib/machine_cert_manager/Exporter.rb
+++
@@ -1,7 +0,0 @@
-module MachineCertManager
-  class Exporter
-    # Initialize with a docker-machine <tt>name</tt>.
-    def initialize name
-    end
-  end
-end
---
+++ lib/machine_cert_manager/exporter.rb
@@ -0,0 +1,7 @@
+module MachineCertManager
+  class Exporter
+    # Initialize with a docker-machine <tt>name</tt>.
+    def initialize name
+    end
+  end
+end
