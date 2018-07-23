b1bedcb59d37e1b2a92475fe58e09423ce00cd99
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2018-07-23 18:32:55 +0200

    Simplify CLI

--- lib/machine_configure/cli.rb
+++ lib/machine_configure/cli.rb
@@ -36,28 +36,52 @@ module MachineConfigure
       end
 
       def handle_export
-        verify_options :name #, :zipfile
-        dm_name = @arguments[:options][:name]
-        zipfile = @arguments[:options][:zipfile]
+        verify_keywords_export
+        dm_name = @arguments[:keywords][:export][1]
+        zipfile = @arguments[:keywords][:export][2]
         zipfile = get_zipfile_from_name dm_name  unless (zipfile)
         exporter = Exporter.new dm_name
         exporter.export_to zipfile
       end
 
       def handle_import
-        verify_options :zipfile
-        zipfile = @arguments[:options][:zipfile]
+        verify_keywords_import
+        zipfile = @arguments[:keywords][:import][1]
         importer = Importer.new
         importer.import_from zipfile
       end
 
       def verify_options *option_names
         option_names.flatten.each do |option_name|
-          unless (@arguments[:options][option_name])
-            error(
-              "Option `--#{VALID_ARGUMENTS[:double][option_name].first.first}' must be given."
-            )
-          end
+          error(
+            "Option `--#{VALID_ARGUMENTS[:double][option_name].first.first}' must be given."
+          )  unless (@arguments[:options][option_name])
+        end
+      end
+
+      def verify_keywords_export
+        error(
+          "Missing argument DOCKER_MACHINE_NAME for #{VALID_ARGUMENTS[:keywords][:export].first.first}."
+        )  unless (@arguments[:keywords][:export][1])
+        verify_keyword_size_for :export
+      end
+
+      def verify_keywords_import
+        error(
+          "Missing argument ZIP_FILE for #{VALID_ARGUMENTS[:keywords][:import].first.first}.",
+          "See --help for more information."
+        )  unless (@arguments[:keywords][:import][1])
+        verify_keyword_size_for :import
+      end
+
+      def verify_keyword_size_for keyword_name
+        target_size = VALID_ARGUMENTS[:keywords][keyword_name].size
+        given_size  = @arguments[:keywords][keyword_name].size
+        if (given_size > target_size)
+          extra_arguments = @arguments[:keywords][keyword_name][target_size .. -1]
+          error(
+            "Invalid argument#{extra_arguments.size > 1 ? ?s : ''} `#{extra_arguments.join(', ')}'"
+          )
         end
       end
 
--- lib/machine_configure/cli_constants.rb
+++ lib/machine_configure/cli_constants.rb
@@ -40,8 +40,8 @@ module MachineConfigure
         ]
       },
       keywords: {
-        export: [['export']],
-        import: [['import']]
+        export: [['export', ?e], :INPUT, :INPUTS],
+        import: [['import', ?i], :INPUTS]
       }
     }
 
@@ -50,35 +50,32 @@ module MachineConfigure
     HELP_TEXT = <<-END_HELP_TEXT
 USAGE
   $ #{cli_name} [--help|--version]
-  $ #{cli_name} export --name [DOCKER_MACHINE_NAME] --zip [ZIP_FILE]
-  $ #{cli_name} import --zip [ZIP_FILE]
+  $ #{cli_name} export DOCKER_MACHINE_NAME [ZIP_FILE]
+  $ #{cli_name} import ZIP_FILE
 
 KEYWORDS
   export
     Export existing configuration files from
-    a --name DOCKER_MACHINE_NAME to a --zip ZIP_FILE.
+    the docker-machine instance DOCKER_MACHINE_NAME.
+    Optionally, add a ZIP_FILE name.
   import
-    Import an exported --zip ZIP_FILE.
+    Import an exported ZIP_FILE.
 
 OPTIONS
   --help -h
     Print this text and exit.
   --version -v
     Print the current version number and exit.
-  --name -n [DOCKER_MACHINE_NAME]
-    The name of an existing docker-machine instance.
-    Use this with `export'.
-  --zip -z [ZIP_FILE]
-    The filepath to a (not necessarily existing) zip archive file.
-    Use this with both `export' and `import'.
 
 EXAMPLES
   Export an existing docker-machine instance with the name "my_machine",
-  to a new zip file "my_machine_configs.zip":
-    $ #{cli_name} export --name my_machine --zip my_machine_configs
+  to a new zip file "my_machine.zip":
+    $ #{cli_name} export my_machine
+  The same as above, but specify the output zip file name:
+    $ #{cli_name} export my_machine my_machine_configs
   Import a new docker-machine instance from
   the zip file "my_machine_configs.zip":
-    $ #{cli_name} import --zip my_machine_configs.zip
+    $ #{cli_name} import my_machine_configs.zip
     END_HELP_TEXT
   end
 end
